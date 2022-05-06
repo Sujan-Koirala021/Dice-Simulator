7c2be3f6b87e6a71e75d0a9c49567da9a9fb8da5
Sujan Koirala <077bct087.sujan@pcamupus.edu.np>
2022-05-06 08:11:57 +0545

    DIce simulation successgit add .

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+# Dice Simulator
+
 from tkinter import *
 from PIL import ImageTk, Image
 import random
 
 root = Tk()
+root.title('Dice Simulator')
 root.geometry('300x300')
+root.resizable(False, False)    # Full screen mode disabled
 
-# Generate Random number from 1 to 6
+
+# Returns pillow image object
+def makeTkImage(fileSource):
+    return ImageTk.PhotoImage(Image.open(fileSource))
+
+# Returns Random number from 1 to 6
 
 
 def generateNumber():
@@ -14,19 +23,42 @@ def generateNumber():
 
 def rollDiceMechanism():
     num = generateNumber()
-    print(num)
-    diceLabel['image'] = dice1Img
+
+    # Changing image attribute of label
+    if num == 1:
+        diceLabel['image'] = diceImg1
+    elif num == 2:
+        diceLabel['image'] = diceImg2
+    elif num == 3:
+        diceLabel['image'] = diceImg3
+    elif num == 4:
+        diceLabel['image'] = diceImg4
+    elif num == 5:
+        diceLabel['image'] = diceImg5
+    else:
+        diceLabel['image'] = diceImg6
+
 
 
 # Image related stuff
-diceImg = ImageTk.PhotoImage(Image.open('images/main.png'))
-diceLabel = Label(image=diceImg)
+
+# Default image initially
+initDiceImg = makeTkImage('images/main.png')
+
+# Dice faces images
+diceImg1 = makeTkImage('images/one.png')
+diceImg2 = makeTkImage('images/two.png')
+diceImg3 = makeTkImage('images/three.png')
+diceImg4 = makeTkImage('images/four.png')
+diceImg5 = makeTkImage('images/five.png')
+diceImg6 = makeTkImage('images/six.png')
+
+# Display default image in label
+diceLabel = Label(image=initDiceImg)
 diceLabel.pack(pady=20)
 
-dice1Img = ImageTk.PhotoImage(Image.open('images/one.png'))
 
-
-# Addition of Button to Roll Dice
+# Button to Roll Dice
 rollDice = Button(root, text="Roll Dice", command=rollDiceMechanism)
 rollDice.pack(pady=20)
 
