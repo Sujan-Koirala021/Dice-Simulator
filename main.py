2b3534e2191c12994a8d4723494d306600e72f8f
Sujan Koirala <077bct087.sujan@pcamupus.edu.np>
2022-05-06 07:44:26 +0545

    Normal image transition on buttonclick success

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from tkinter import *
-
+from PIL import ImageTk, Image
 import random
 
+root = Tk()
+root.geometry('300x300')
+
 # Generate Random number from 1 to 6
+
 
 def generateNumber():
     return random.randint(1, 6)
@@ -11,13 +15,19 @@ def generateNumber():
 def rollDiceMechanism():
     num = generateNumber()
     print(num)
+    diceLabel['image'] = dice1Img
 
 
-root = Tk()
-num = generateNumber()
-print(num)
+# Image related stuff
+diceImg = ImageTk.PhotoImage(Image.open('images/main.png'))
+diceLabel = Label(image=diceImg)
+diceLabel.pack(pady=20)
 
+dice1Img = ImageTk.PhotoImage(Image.open('images/one.png'))
+
+
+# Addition of Button to Roll Dice
 rollDice = Button(root, text="Roll Dice", command=rollDiceMechanism)
-rollDice.pack()
+rollDice.pack(pady=20)
 
 root.mainloop()
